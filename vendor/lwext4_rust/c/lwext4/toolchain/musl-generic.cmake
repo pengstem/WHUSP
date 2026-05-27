9fee361e13b4f02048366c45c8a7846e6a886e9d
Nastem <pengsteam16@gmail.com>
2026-05-28 00:46:42 +0800

    chore(lwext4): change the flag to avoid some problems

--- vendor/lwext4_rust/c/lwext4/toolchain/musl-generic.cmake
+++ vendor/lwext4_rust/c/lwext4/toolchain/musl-generic.cmake
@@ -80,6 +80,12 @@ SET(CMAKE_C_FLAGS_DEBUG "-O0 -g -ggdb3" CACHE INTERNAL "c debug compiler flags")
 SET(CMAKE_CXX_FLAGS_DEBUG "-O0 -g -ggdb3" CACHE INTERNAL "cxx debug compiler flags")
 SET(CMAKE_ASM_FLAGS_DEBUG "-g -ggdb3" CACHE INTERNAL "asm debug compiler flags")
 
-SET(CMAKE_C_FLAGS_RELEASE "-O2 -g -ggdb3" CACHE INTERNAL "c release compiler flags")
+if (ARCH STREQUAL "loongarch64")
+    # Official contest Docker's GCC 13.2 LoongArch build has produced corrupted
+    # executable loads from lwext4 at -O2, surfacing as user SIGILL under QEMU.
+    SET(CMAKE_C_FLAGS_RELEASE "-O1 -g -ggdb3" CACHE INTERNAL "c release compiler flags")
+else()
+    SET(CMAKE_C_FLAGS_RELEASE "-O2 -g -ggdb3" CACHE INTERNAL "c release compiler flags")
+endif()
 SET(CMAKE_CXX_FLAGS_RELEASE "-O2 -g -ggdb3" CACHE INTERNAL "cxx release compiler flags")
 SET(CMAKE_ASM_FLAGS_RELEASE "" CACHE INTERNAL "asm release compiler flags")
